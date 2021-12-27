f0cb5f0f7dbda54d5d0ccdfbcc039c42188cb319
Habbler <habbler@online.de>
2021-12-27 16:55:19 +0100

    First working version

--- app/Main.hs
+++ app/Main.hs
@@ -21,12 +21,19 @@ main = do
 
 setup :: Window -> UI ()
 setup w = void $ do
+    addJavaScript w "https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"
     return w # set title "Buttons"
     UI.addStyleSheet w "buttons.css"
+    addStyleSheetExt w "https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css"
 
     buttons <- mkButtons
+    let myTab = mkTable [string "H1", string "H2"]
+                     [ [string "R1C1", string "R1C2"]
+                     , [string "R2C1", string "R2C2"] ] # set UI.id_ "example" # set UI.class_ "display"  
+                   
+    dgScript <- mkElement  "Script" # set UI.text "$(document).ready(function() { $('#example').DataTable(); } );" 
     getBody w #+
-        [UI.div #. "wrap" #+ (greet ++ map element buttons ++ [viewSource])]
+        [UI.div #. "wrap" #+ (greet ++ [myTab, element dgScript] ++ map element buttons ++ [viewSource])]
 
 greet :: [UI Element]
 greet =
@@ -77,3 +84,33 @@ viewSource = UI.p #+
     where
     url = samplesURL ++ "Buttons.hs"
 
+addJavaScript :: UI.Window -> FilePath -> UI ()
+addJavaScript w pathName = void $ do
+  el <- UI.mkElement "script" # set UI.src pathName
+  UI.getHead w #+ [UI.element el]
+
+addStyleSheetExt :: Window -> FilePath -> UI ()
+addStyleSheetExt w filename = void $ do
+    el <- mkElement "link"
+            # set (attr "rel" ) "stylesheet"
+            # set (attr "type") "text/css"
+            # set (attr "href") (filename)
+    getHead w #+ [element el]  
+
+mkTable    :: [UI Element]-> [[UI Element]] -> UI Element
+mkTable mhead mrows = do
+        rowHeader <- wrap "tr" =<< mapM (\entry -> wrap "th" [entry] ) =<< sequence mhead
+
+        rows0 <- mapM sequence mrows
+
+        rows  <- forM rows0 $ \row0 -> do
+            wrap "tr" =<< forM row0 (\entry -> wrap "td" [entry])
+
+        th <- wrap "thead" [rowHeader]    
+        tb <- wrap "tbody" rows
+ 
+        wrap "table" [th, tb]
+
+    where
+    wrap :: String -> [Element] -> UI Element    
+    wrap c xs = mkElement c #+ map element xs  
